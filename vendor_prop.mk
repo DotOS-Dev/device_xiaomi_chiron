c2983a48524d47d16e727f5c69267b5ea3fc30c3
ljb233 <18281544030@163.com>
2020-02-20 15:37:30 +0800

    dotos-p for chiron

---
+++ vendor_prop.mk
@@ -0,0 +1,9 @@
+# Sensors
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.qti.sensors.noneui=true \
+    ro.xiaomi.sar_fusion=true
+
+# Display
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.sf.lcd_density=403 \
+    vendor.display.disable_avr=1
